e1ff4b447a7102ee50047508d11488f0d13f783b
shravanighule <shravanighule16@gmail.com>
2024-08-27 10:34:56 +0530

    Add files via upload

---
+++ even no in loops.cpp
@@ -0,0 +1,32 @@
+//shravanighule 056
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+
+    int num1[] = { 2, 7, 6, 5, 4 };
+    
+    int n = 5;
+    for (int i = 0; i < n; i++) {
+
+        if (num1[i] % 2 == 0) {
+            cout << num1[i] << " ";
+        }
+    }
+ 
+    return 0;
+}
+
+
+
+
+/* 2 6 4
+--------------------------------
+Process exited after 0.09853 seconds with return value 0
+Press any key to continue . . .
+
+*/
+
+
+
