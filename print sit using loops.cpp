4ba8da721e6528554e0a0553bfaa88322623be81
shravanighule <shravanighule16@gmail.com>
2024-08-29 06:57:50 +0530

    Add files via upload

---
+++ print sit using loops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+for (int i = 1; i <= 5; ++i) {
+cout << "SIT" << " ";
+}
+return 0;
+}
+
+
+
+SIT SIT SIT SIT SIT
+--------------------------------
+Process exited after 0.2259 seconds with return value 0
+Press any key to continue . . .
+
+
+
+
